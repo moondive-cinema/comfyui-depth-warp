2d938d8fb512f6babe983a3233dbcac9bf27b21d
moondive-cinema <moondive79@gmail.com>
2026-03-12 13:40:16 +0900

    fix: use importlib to support hyphenated directory name

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
-from .depth_warp_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+import importlib.util, os
+
+_here = os.path.dirname(__file__)
+_spec = importlib.util.spec_from_file_location(
+    "depth_warp_node",
+    os.path.join(_here, "depth_warp_node.py")
+)
+_mod = importlib.util.module_from_spec(_spec)
+_spec.loader.exec_module(_mod)
+
+NODE_CLASS_MAPPINGS       = _mod.NODE_CLASS_MAPPINGS
+NODE_DISPLAY_NAME_MAPPINGS = _mod.NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
